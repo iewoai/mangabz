c8ddc764fa01138d4b5126ebf74875bd000da050
iewoai <1722685076@qq.com>
2021-02-28 23:29:53 +0800

    initail mangabz project

---
+++ comic/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    import requests
+    import json
+
+    api_request = requests.get("https://api.github.com/users?since=135")
+    api = json.loads(api_request.content)
+    return render(request, "home.html", {"api": api})
---
+++ static/js/comicindex.js
@@ -0,0 +1,25 @@
+// $(document).ready(function () {
+
+// });
+var beforeIndex = 0;
+$("#hotCatgoryId .list-con-title-class a").hover(function () {
+    $("#hotCatgoryId .list-con-title-class a").removeClass("active");
+    $(this).addClass("active");
+    var id = $("#hotCatgoryId .list-con-title-class a").index(this);
+    if (beforeIndex != id)
+         $($("#hotCatgoryId .index-manga-list")[beforeIndex]).hide(400);
+    $($("#hotCatgoryId .index-manga-list")[id]).show(400);
+    beforeIndex = id;
+    $("#hotCatgoryId .list-con-title-more").attr("href", $(this).attr("href"));
+}, function () {});
+
+$(".rank-list .list").hover(function () {
+    var tempObj = $(".rank-list .list.rank-item-1");
+    $(tempObj).removeClass("rank-item-1");
+    $(tempObj).addClass("rank-item-2");
+    $(tempObj).find("img").hide();
+
+    $(this).removeClass("rank-item-2");
+    $(this).addClass("rank-item-1");
+    $(this).find("img").show();
+}, function () { });
